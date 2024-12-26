e6234063b18063eb846ad3a9330f9d85a9b37851
Flier Lu <flier.lu@gmail.com>
2024-12-26 11:09:48 +0800

    remove Eunomo

--- .formatter.exs
+++ .formatter.exs
@@ -1,10 +1,4 @@
 # Used by "mix format"
 [
-  plugins: [Eunomo],
-  eunomo_opts: [
-    sort_alias: true,
-    sort_import: true,
-    sort_require: true
-  ],
   inputs: ["{mix,.formatter}.exs", "{config,lib,test}/**/*.{ex,exs}"]
 ]
