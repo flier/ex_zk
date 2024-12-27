025bb3da6d2ccb1e22b57d45f7961efd92265bbe
Flier Lu <flier.lu@gmail.com>
2024-12-27 13:21:33 +0800

    move constructors to Op

--- lib/ex_zk/multi.ex
+++ lib/ex_zk/multi.ex
@@ -48,6 +48,24 @@ defmodule ExZk.Multi do
             | GetChildrenResponse.t()
             | GetDataResponse.t()
 
+    @spec create(Path.t(), data :: binary(), opts :: [Create.option()]) :: Op.t()
+    def create(path, data \\ "", opts \\ []), do: {:create, path, data, opts}
+
+    @spec check_version(Path.t(), version :: integer()) :: Op.t()
+    def check_version(path, version \\ 0), do: {:check, path, version}
+
+    @spec delete(Path.t(), version :: integer()) :: Op.t()
+    def delete(path, version \\ 0), do: {:delete, path, version}
+
+    @spec get_children(Path.t()) :: Op.t()
+    def get_children(path), do: {:get_children, path}
+
+    @spec get_data(Path.t()) :: Op.t()
+    def get_data(path), do: {:get_data, path}
+
+    @spec set_data(Path.t(), data :: binary(), version :: integer()) :: Op.t()
+    def set_data(path, data, version \\ 0), do: {:set_data, path, data, version}
+
     @spec new_request(t()) :: {OpCode.t(), request()}
 
     def new_request({:create, path, data, ops}),
@@ -169,7 +187,6 @@ defmodule ExZk.Multi do
   end
 
   @type request :: [MultiHeader.t() | Op.request()]
-  @type response :: [MultiHeader.t() | Op.response()]
 
   ####
   ## Public API
@@ -186,24 +203,6 @@ defmodule ExZk.Multi do
     |> Stream.concat([done()])
     |> Enum.into([])
   end
-
-  @spec create(Path.t(), data :: binary(), opts :: [Create.option()]) :: Op.t()
-  def create(path, data \\ "", opts \\ []), do: {:create, path, data, opts}
-
-  @spec check_version(Path.t(), version :: integer()) :: Op.t()
-  def check_version(path, version \\ 0), do: {:check, path, version}
-
-  @spec delete(Path.t(), version :: integer()) :: Op.t()
-  def delete(path, version \\ 0), do: {:delete, path, version}
-
-  @spec get_children(Path.t()) :: Op.t()
-  def get_children(path), do: {:get_children, path}
-
-  @spec get_data(Path.t()) :: Op.t()
-  def get_data(path), do: {:get_data, path}
-
-  @spec set_data(Path.t(), data :: binary(), version :: integer()) :: Op.t()
-  def set_data(path, data, version \\ 0), do: {:set_data, path, data, version}
 
   ####
   ## Private methods
